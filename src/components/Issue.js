59406cf0ccb74f773bcdd14187fbacc88509056d
Lila Sarfson <lilamandarina@hotmail..com>
2022-08-25 14:13:02 +0200

    cerrar formulario y todo lo relacionado con esto

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -15,11 +15,22 @@ function Issue (props){
     const getDescription= (e) =>{
         setDescription(e.target.value);
     }
+    const closeForm = ()=>{
+        if(issue.length >= 0){
+            setActive(false)
+        }
+    }
+    const deleteIssue = (e)=>{
+        // Hacer un filtro mirando el indice que coincide con el target y quedarse con todos menos ese
+        let issueToDelete = (e.target.value)
+        console.log(issueToDelete)
+    }
     const handleClick=(e)=>{
         e.preventDefault();
         let newIssue = {
             name:name,
             description:description, 
+            id:issue.length
         }
         setIssue((issue)=>[...issue, newIssue])
         console.log(active);
@@ -39,26 +50,29 @@ function Issue (props){
             : active?
             <div className='issue-container-form'>
                 <form className = 'newIssue-form'>
+                <img style={{width:'20px', alignSelf: 'end', padding:'0.5rem'}}
+                src='cross.png' alt='cross' onClick={closeForm}></img>
                 <label for="issue">Issue</label>
                 <input className='issue-input' onChange={getName}
                  type="text" id="issue" name="newIssue"/> <br/>
-
-                 <label for="description">Description</label>
-                <textarea onChange={getDescription}
-                rows="6" cols="20"
-                  id="issue-description" name="newIssueDescription"/>
-
-
-                {/* 
-                https://es.stackoverflow.com/questions/466976/enviar-datos-de-componente-hijo-a-padre-react-js
-                */}
-                <label for="user">who?</label>
-                <select id="user" name="user">
-                    {props.user.map((user) =>
-                    <option key={user.id} value=''>{user.name}</option>)
-                    }
-                </select><br/>
-
+            <div className='descWho-container'>   
+                <section>           
+                    <label for="description">Description</label><br/>
+                    <textarea onChange={getDescription}
+                    rows="6" cols="20"
+                    id="issue-description" 
+                    name="newIssueDescription"/>
+                  </section>
+                  <section>
+                    <label for="user">who?</label>
+                    <select id="user" name="user">
+                        {props.user.map((user) =>
+                        <option key={user.id} value=''>{user.name}</option>)
+                        }
+                    </select>
+                </section>
+            </div>
+    
                 <input onClick={handleClick}
                 className='add-issue'
                 value="submit form"
@@ -69,7 +83,7 @@ function Issue (props){
             ''
             }
             <div className="main-issues">
-                { issue.length===0 ?
+                { issue.length===0 || active ?
                 ''
                 :
                 <button className='add-issue' onClick={() =>{
@@ -80,6 +94,11 @@ function Issue (props){
                 <div key={issue.length + 1} className='issue-container'>
                     <h3 className='issue-title'>{issue.name}</h3>
                     <p>{issue.description}</p>
+                    <div>
+                        <img onClick={deleteIssue} style={{width: '7%'}} src='eliminar.png' alt='eliminar'></img>
+                        <img style={{width: '7%'}} src='editar.png' alt='eliminar'></img>
+
+                    </div>
                     <User name={(props.user).name} size={"small"}/>
 
                 </div>)}
